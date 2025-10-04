29c4e8bb4c15f2bdac1fed4dd300087fb93fcfeb
mannilakash <mannilakash@gmail.com>
2025-10-04 16:40:53 -0400

    adding contact us and footer details

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import LogoSection from "./sections/LogoSection"
 import FeatureCards from "./sections/FeatureCards"
 import ExperienceSection from "./sections/ExperienceSection"
 import TechStack from "./sections/TechStack"
+import Contact from "./sections/Contact"
+import Footer from "./sections/Footer"
 
 const App = () => {
   return (
@@ -16,6 +18,8 @@ const App = () => {
         <FeatureCards/>
         <ExperienceSection/>
         <TechStack/>
+        <Contact/>
+        <Footer/>
     </>
   )
 }
