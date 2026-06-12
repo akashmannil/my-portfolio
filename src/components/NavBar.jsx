253428603e5b3f5d2fe118892bbbc9e6acadb88e
mannilakash <mannilakash@gmail.com>
2026-06-12 13:45:32 -0400

    fix: stash issue

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,40 +1,7 @@
-<<<<<<< Updated upstream
-import React from 'react'
-import { navLinks } from '../constants'
-=======
->>>>>>> Stashed changes
 import { useEffect, useState } from 'react';
 import { navLinks } from '../constants';
 
 const NavBar = () => {
-<<<<<<< Updated upstream
-    const [scrolled, setScrolled] = useState(false);
-    useEffect(() => {
-        const handleScroll = () => {
-            const isScrolled = window.scrollY > 10;
-            setScrolled(true);
-        };
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    });
-    const handleDownload = (e) => {
-        if (e.currentTarget.getAttribute('href') != "") {
-            return;
-        }
-        const link = document.createElement('a');
-        link.href = '/Akash-Mannil-Restaurant-Resume.docx';
-        link.download = 'Akash_M_Resume.docx'; // customize the filename
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    };
-    return (
-        <header className={`navbar ${scrolled ? 'scrolled' : 'not-scrolled'}`}>
-            <div className='inner'>
-                <a className='logo' href="#hero">
-                    Akash | AM
-                </a>
-=======
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
@@ -42,7 +9,6 @@ const NavBar = () => {
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
->>>>>>> Stashed changes
 
   const handleDownload = (e) => {
     if (e.currentTarget.getAttribute('href') !== '') return;
